850b18c86f321484ed25bba8d9449ab766de9a5f
syedpeermuhaideen <47441256+syedpeermuhaideen@users.noreply.github.com>
2019-02-08 15:13:59 +0530

    Update vowel_or_consonent.py

--- vowel_or_consonent.py
+++ vowel_or_consonent.py
@@ -1,5 +1,8 @@
 a=raw_input()
-if(a=='a' or a=='e' or a=='i'or a=='o' or a=='u'):
+if (a.isalpha()):
+ if(a=='a' or a=='e' or a=='i'or a=='o' or a=='u'):
   print("Vowel")
-else:
+ else:
   print("Consonent")
+else:
+  print("invalid")
