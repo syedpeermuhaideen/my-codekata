21ee2c5ab4210432e85fe5e67add8477c4450186
syedpeermuhaideen <47441256+syedpeermuhaideen@users.noreply.github.com>
2019-02-18 14:48:46 +0530

    Create prime_interval.py

---
+++ prime_interval.py
@@ -0,0 +1,13 @@
+lower,upper=input().split(" ")
+lower = int(lower)
+upper = int(upper)
+
+
+
+for num in range(lower,upper):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num,end=" ")
